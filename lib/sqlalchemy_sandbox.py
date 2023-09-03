cb35e130d5e03339fa18abd899cff8786907ceb9
Beliya470 <anneanziay78@gmail.com>
2023-09-03 22:00:25 +0300

    Completed Assignment

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 
+# Necessary imports
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base  # Updated import
 
+# Create an instance of the declarative base class
 Base = declarative_base()
 
+# Define the Student class which represents the 'students' table
 class Student(Base):
-    pass
+    __tablename__ = 'students'
 
+    # Define columns for the table
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+# Define the Course class which represents the 'courses' table
+class Course(Base):
+    __tablename__ = 'courses'
+
+    # Define columns for the table
+    id = Column(Integer(), primary_key=True)
+    course_name = Column(String())
+
+# Only run the following block if this script is the main program
 if __name__ == '__main__':
-    pass
+    # Create an SQLite engine and point it to the 'students.db' file
+    engine = create_engine('sqlite:///students.db')
+
+    # Create all tables associated with our Base class (in this case, the 'students' and 'courses' tables)
+    Base.metadata.create_all(engine)
